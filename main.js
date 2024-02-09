7259b01ca86ed2442252a255180207d9206d39ac
Hazem agil <21099@ppu.edu.ps>
2024-02-09 22:36:45 +0200

    Api using axios

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-async function getProduct(){
-    const response= await fetch("https://dummyjson.com/products");
-    const data =  await response.json();
-    const results= data.products.map( function (ele) {
-        return `
-        <div class="all">
-        <div class="proimg">
-        <img src="${ele.thumbnail}" alt="this is product img"/>
-        <div class="proinfo">
-        <h1>${ele.id}</h1>
-        <h1>${ele.title}</h1>    
-        <span>${ele.price}$</span>
-        </div>
-        </div>
-        </div>
-        
-        `
-    } ).join('');
-    document.querySelector(".ProFromJs").innerHTML=results;
-
+async function getProduct() {
+    try {
+        const response = await axios.get("https://dummyjson.com/products");
+        const data = response.data;
+        const results = data.products.map(function (ele) {
+            return `
+            <div class="all">
+                <div class="proimg">
+                    <img src="${ele.thumbnail}" alt="this is product img"/>
+                </div>
+                <div class="proinfo">
+                    <h1> id: ${ele.id}</h1>
+                    <h1> Name: ${ele.title}</h1>    
+                    <span class=labelprice> Price: ${ele.price}$</span>
+                    <button class="btndetalis">
+                        <a href="productInfo.html?product_id=${ele.id}">More-Detalis</a>
+                    </button>
+                </div>
+            </div>
+            `;
+        }).join('');
+        document.querySelector(".ProFromJs").innerHTML = results;
+    } catch (error) {
+        console.error('Error', error);
+    }
 }
+
 getProduct();
